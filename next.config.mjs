1b620767d7c7fd2f8c99d3a546d463383ca104f6
Emin Metin <eminsteam58@hotmail.com>
2024-03-16 09:46:39 +0300

    car showcase

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'cdn.imagin.studio',
+        protocol: 'https',
+        port: '',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
